17b753cf932e28a2a5e105c5985798d8e67873b4
Thomas Maierbacher <maierbacher@gmail.com>
2018-01-01 21:23:03 +0100

    use a iterator implementation

--- src/lib.rs
+++ src/lib.rs
@@ -4,20 +4,26 @@ pub mod book_shorter {
 
     pub const TARGET: isize = 81;
 
-    struct Word<'a> {
-        w: &'a str
+    pub struct LineIterator<'a> {
+        sorted: Vec<Vec<&'a str>>,
     }
 
-    fn load_data(book_in: &str) -> Vec<Vec<Word>> {
-        let mut vec: Vec<Vec<Word>> = vec![Vec::new()];
-        for x in book_in.split_whitespace() {
-            let cnt = x.chars().count();
-            while vec.len() < cnt {
-                vec.push(Vec::new())
+    impl<'a> LineIterator<'a> {
+
+        /// Constructs a new Line Iterator
+        ///
+        /// book_in: string containing the book to be reformated
+        pub fn new(book_in: & str) -> LineIterator {
+            let mut vec: Vec<Vec<&str>> = vec![Vec::new()];
+            for x in book_in.split_whitespace() {
+                let cnt = x.chars().count();
+                while vec.len() < cnt {
+                    vec.push(Vec::new())
+                }
+                vec[cnt - 1].push(x);
             }
-            vec[cnt - 1].push(Word{w: x});
+            LineIterator { sorted: vec }
         }
-        vec
     }
 
     enum FillResult {
@@ -34,19 +40,54 @@ pub mod book_shorter {
         i as usize - 2
     }
 
-    pub const MAX_COMBINATIONS: usize = 3;
+    impl<'a> Iterator for LineIterator<'a> {
+        type Item = String;
 
+        /// get next reformated line
+        fn next(& mut self) -> Option<String> {
+            let mut fill_level = 0;
+            let mut res = String::new();
+            for i in (0..self.sorted.len()).rev() {
+                while self.sorted[i].len() > 0 {
+                    match fill_line(&mut self.sorted, i, fill_level, &mut res) {
+                        FillResult::Done => {
+                            // good
+                            while fill_level > 0 {
+                                res.push(' ');
+                                fill_level -= 1;
+                            }
+                            // replace last whitespace with newline
+                            res.pop();
+                            res.push('\n');
+                            return Some(res);
+                        }
+                        FillResult::Failed(_) => {
+                            // filed to find a optimal solution. Find a good suboptimal solution
+                            fill_level += 1;
+                            println!("Failed to fill a line");
+                        }
+                    }
+                }
+            }
+            None
+        }
+    }
 
-    fn fill_line(sorted: & mut Vec<Vec<Word>>, in_idx: usize, current_fill_level: isize, res: &mut String) -> FillResult {
+    fn fill_line(sorted: &mut Vec<Vec<&str>>,
+                 in_idx: usize,
+                 current_fill_level: isize,
+                 res: &mut String)
+                 -> FillResult {
         let mut idx = in_idx;
         let free = TARGET - current_fill_level;
+
         while sorted[idx].len() == 0 && idx > 0 {
             idx -= 1;
         }
 
         if sorted[idx].len() == 0 {
             // done, no more data
-            return FillResult::Done
+            return FillResult::Done;
         }
 
         if free <= word_len(idx) {
@@ -54,22 +95,22 @@ pub mod book_shorter {
             let required = free;
             if required >= 2 && sorted[get_idx(required)].len() > 0 {
                 // done - we filled a line
-                res.push_str(sorted[get_idx(required)].pop().unwrap().w);
+                res.push_str(sorted[get_idx(required)].pop().unwrap());
                 res.push(' ');
-                return FillResult::Done
+                return FillResult::Done;
             } else {
-                // not a possible solution
-                return FillResult::Failed(required)
+                // no optimal solution possible
+                return FillResult::Failed(required);
             }
         } else {
             loop {
                 let candidate = sorted[idx].pop().unwrap();
                 match fill_line(sorted, idx, current_fill_level + word_len(idx), res) {
                     FillResult::Done => {
-                        res.push_str(candidate.w);
+                        res.push_str(candidate);
                         res.push(' ');
                         return FillResult::Done;
-                    },
+                    }
                     FillResult::Failed(sub_word_len) => {
                         // put candidate back
                         sorted[idx].push(candidate);
@@ -85,39 +126,10 @@ pub mod book_shorter {
                         } else {
                             return FillResult::Failed(sub_word_len);
                         }
-                        assert!( sorted[idx].len() != 0);
-                    }
-                }
-            };
-        }
-    }
-
-    pub fn compress(book_in: &str) -> String {
-        let mut sorted = load_data(book_in);
-        let mut res: String = String::new();
-
-        let mut fill_level = 0;
-        for i in (0..sorted.len()).rev() {
-            while sorted[i].len() > 0 {
-                match fill_line( &mut sorted, i, fill_level, &mut res) {
-                    FillResult::Done => {
-                        // good
-                        while fill_level > 0 {
-                            res.push(' ');
-                            fill_level -= 1;
-                        }
-                        // replace last whitespace with newline
-                        res.pop();
-                        res.push('\n');
-                    },
-                    FillResult::Failed(sub_word_len) => {
-                        // wo found no optimal solution to fill the line, try with an unfilled character
-                        fill_level += 1;
-                        println!("Failed to fill a line");
+                        assert!(sorted[idx].len() != 0);
                     }
                 }
             }
         }
-        res
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ extern crate gloa;
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        println!("usage: {} inputfile", args[0]);
+        println!("usage: {} <inputfile>", args[0]);
+        return;
     }
     let filename = &args[1];
     let mut f = File::open(filename).expect("File not found");
@@ -19,10 +20,13 @@ fn main() {
     //println!("Longest word in poem {}", shorter::book_info::longest_word(&book_in));
     //println!("Minimum number of lines {}", shorter::book_info::minimum_lines_possible(&book_in));
 
-    let result = gloa::book_shorter::compress(&book_in);
-    println!("Lines in result: {}", result.chars().count() as f32 / 81.);
-
     let mut out = File::create("out.txt").expect("Error creating output file");
-    out.write_all(result.as_bytes()).expect("Error writing output");
-    //print!("{}", result);
+
+    let mut line_cnt = 0;
+    let ln_iterator = gloa::book_shorter::LineIterator::new(&book_in);
+    for s in ln_iterator {
+        out.write_all(s.as_bytes()).expect("Error writing output");
+        line_cnt += 1;
+    }
+    println!("Lines in result: {}", line_cnt);
 }
